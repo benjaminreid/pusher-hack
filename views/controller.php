ca78a44c3904a13b50ea0eaa6c4f64d0bf74b1a3
David Darnes <me@daviddarnes.com>
2015-02-17 20:12:56 +0000

    Formatting styles, button positioning

--- views/controller.php
+++ views/controller.php
@@ -1,13 +1,11 @@
 <?php include 'partials/header.php' ?>
 <header class="header  question">
-  <input type="text" placeholder="Enter your question">
+  <input class="question" type="text" placeholder="Enter your question">
   <input type="submit">
 </header>
 <section class="section  results">
   <div class="yes">
-    <span class="">
-      <h2>Yep!</h2>
-    </span>
+    <h2>Yep!</h2>
   </div>
   <div class="no">
     <h2>Nope!</h2>
--- views/voter.php
+++ views/voter.php
@@ -1,17 +1,17 @@
 <?php include 'partials/header.php' ?>
 <header class="header  question">
-  <h1>Does this work?</h1>
+  <h1 class="question">Does this work?</h1>
 </header>
 <section class="section  voters">
   <div class="yes">
-    <button class="button">
-      <span>Yep!</span>
-    </button>
+    <label class="label">
+      <button class="button">Yep!</button>
+    </label>
   </div>
   <div class="no">
-    <button class="button">
-      <span>Nope!</span>
-    </button>
+    <label class="label">
+      <button class="button">Nope!</button>
+    </label>
   </div>
 </section>
 <?php include 'partials/footer.php' ?>
