1725514b2caacc9acd2114e1f46aca79f3041d6d
Robert Rhoades <opsrob@gmail.com>
2015-02-17 21:43:06 +0000

    Adds caching interface

--- bootstrap.php
+++ bootstrap.php
@@ -10,3 +10,6 @@ $app_key = 'ccc01bbde007761691f4';
 $app_secret = '95a0d4af6cb2d5f418be';
 
 $pusher = new Pusher($app_key, $app_secret, $app_id);
+
+$cache = new Gregwar\Cache\Cache();
+$cache->setCacheDirectory('cache');
