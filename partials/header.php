9c3403726d32af98cff192f01652d21cbe378dc0
David Darnes <me@daviddarnes.com>
2015-02-17 19:32:48 +0000

    Initial commit of front-end branch

--- partials/header.php
+++ partials/header.php
@@ -3,7 +3,8 @@
   <head>
     <meta charset="utf-8" />
     <title>Page Title</title>
-    <link rel="stylesheet" type="text/css" href="/css/styles.css" />
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.2/normalize.min.css" />
+      <link rel="stylesheet" type="text/css" href="/css/styles.css" />
     <!--[if IE]>
       <script src="/js/html5shiv.js"></script>
     <![endif]-->
