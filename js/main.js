075301444cd2d847d861ac7e65561be6589a8b4f
Benjamin Reid <ben@simpleweb.co.uk>
2015-02-17 20:11:41 +0000

    Add vote sub

--- js/main.js
+++ js/main.js
@@ -1,32 +1,47 @@
 (function() {
   $(document).ready(function() {
     var APP_KEY = 'ccc01bbde007761691f4';
-
     var pusher = new Pusher(APP_KEY);
+
+    // channels
+    var channels = {
+      vote: pusher.subscribe('vote')
+    };
 
     var ractive = new Ractive({
       el: '#container',
       template: '#ractive',
       data: { name: 'World!' }
     });
-  });
+    var send = function(channel, event, data) {
+      var payload = {
+        channel: channel,
+        event: event,
+        data: data
+      };
 
-  var send = function(channel, event, data) {
-    var payload = {
-      channel: channel,
-      event: event,
-      data: data
+      $.ajax({
+        url: '/send',
+        type: 'POST',
+        data: payload,
+      }).success(function(res) {
+        console.log(res);
+      }).error(function(e) {
+        console.log(e);
+      });
     };
 
-    $.ajax({
-      url: '/send',
-      type: 'POST',
-      data: payload,
-    }).success(function(res) {
-      console.log(res);
-    }).error(function(e) {
-      console.log(e);
+    var when = function(channel, event, fn) {
+      console.log(channels[channel], channel, channels);
+      channels[channel].bind(event, function(data) {
+        fn(data);
+      });
+    };
+
+    when('vote', 'send-vote', function(data) {
+      console.log(data);
     });
-  };
-  send('foo-channel', 'foo-event', { foo: 'bar' });
+
+    send('vote', 'send-vote', { vote: 'yes' });
+  });
 })();
