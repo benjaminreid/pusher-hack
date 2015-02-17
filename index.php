68eae6327347958900011e704377192ea1b3c7bf
Robert Rhoades <opsrob@gmail.com>
2015-02-17 19:58:04 +0000

    Adds send endpoint

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 require 'bootstrap.php';
 
 $app->get('/controller', function() {
-  return require 'views/controller.php';
+
+    include 'views/controller.php';
+
+    // Return 200
+    return new Response();
+});
+
+$app->get('/', function() {
+    include 'views/voter.php';
+
+    return new Response();
+});
+
+$app->post('/send', function(Request $request) {
+
+    error_log(var_export($request->request->all(), true));
+
+    return new Response();
+
 });
 
 $app->run();
