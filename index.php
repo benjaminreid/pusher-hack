075e5a675c30dc0ce2c87e031294c9f5e75c3dcc
Robert Rhoades <opsrob@gmail.com>
2015-02-17 19:37:41 +0000

    Fixes stray 1. Adds voter route

--- index.php
+++ index.php
@@ -1,9 +1,21 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
+
 require 'bootstrap.php';
 
 $app->get('/controller', function() {
-  return require 'views/controller.php';
+
+    include 'views/controller.php';
+
+    // Return 200
+    return new Response();
+});
+
+$app->get('/', function() {
+    include 'views/voter.php';
+
+    return new Response();
 });
 
 $app->run();
